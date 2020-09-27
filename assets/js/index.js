b7c8ab8290425d60e5c99882a4f087f3fcc60292
wilsonng21 <wilsongch92@gmail.com>
2020-09-27 19:04:30 -0400

    form to add new job rendering

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
-const cardContainer = document.querySelector('div.row')
+const cardContainer = document.querySelector('div.row.cards')
+const homeOwnerBtn = document.querySelector('button.btn.homeowner')
+const form = document.querySelector('form')
 
 fetch('http://localhost:3000/jobs')
     .then(response => response.json())
@@ -7,6 +9,65 @@ fetch('http://localhost:3000/jobs')
             turnJobIntoCard(job)
         })
     })
+
+// const handleCreateUser = (username) => {
+//     fetch('')
+// }   
+
+homeOwnerBtn.addEventListener('click', event => {
+    let formGroupA = document.createElement('div')
+    formGroupA.classList.add('form-group')
+    let usernameLabel = document.createElement('label')
+    usernameLabel.setAttribute('for', 'username')
+    usernameLabel.innerText = 'Username'
+    let usernameInput = document.createElement('input')
+    usernameInput.setAttribute('id', 'username')
+    usernameInput.type = 'text'
+    usernameInput.classList.add('form-control')
+
+    let formGroupB = document.createElement('div')
+    formGroupB.classList.add('form-group')
+    let zipcodeLabel = document.createElement('label')
+    zipcodeLabel.setAttribute('for', 'zipcode')
+    zipcodeLabel.innerText = 'Zipcode'
+    let zipcodeInput = document.createElement('input')
+    zipcodeInput.setAttribute('id', 'zipcode')
+    zipcodeInput.type = 'text'
+    zipcodeInput.classList.add('form-control')
+
+    let formGroupC = document.createElement('div')
+    formGroupC.classList.add('form-group')
+    let descriptionLabel = document.createElement('label')
+    descriptionLabel.setAttribute('for', 'description-text')
+    descriptionLabel.innerText = 'Description'
+    let descriptionText = document.createElement('textarea')
+    descriptionText.setAttribute('id', 'description-text')
+    descriptionText.type = 'text'
+    descriptionText.style = 'width: 300px'
+    descriptionText.classList.add('form-control')
+
+    let submitBtn = document.createElement('button')
+    submitBtn.type = 'submit'
+    submitBtn.classList.add('btn', 'btn-primary', 'mb-2')
+    submitBtn.innerText = 'submit'
+
+    formGroupA.append(usernameLabel, usernameInput)
+    formGroupB.append(zipcodeLabel, zipcodeInput)
+    formGroupC.append(descriptionLabel, descriptionText)
+    form.innerHTML = ''
+    form.append(formGroupA, formGroupB, formGroupC, submitBtn)
+
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+
+        let username = event.target.username.value
+        let zipcode = event.target.zipcode.value
+        let jobDescription = event.target['description-text'].value
+
+
+        
+    })
+})    
 
 const renderBids = (job, parentNode) => {
     let cardBorder = document.createElement('div')
